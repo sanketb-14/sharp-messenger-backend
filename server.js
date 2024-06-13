1d7920385ee37abab5fc1bcf0a0e736ab2519511
sanket <sanketbarapatre999@gmail.com>
2024-06-13 21:55:00 +0530

    initial commit

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ const app = express();
 
 //Set security HTTP Headers
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+  origin: '*' // Allow requests from any origin
+}))
 
 // Prevent XSS attacks
 
