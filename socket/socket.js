39a024eb2b6a57bb7e01aa5d78c832319d59d39d
sanket <sanketbarapatre999@gmail.com>
2024-07-10 16:49:54 +0530

    completed backend

--- socket/socket.js
+++ socket/socket.js
@@ -6,7 +6,7 @@ export const app = express();
 export const server = http.createServer(app);
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://sharp-messenger.vercel.app",
     methods: ["GET", "POST"],
   },
 });
